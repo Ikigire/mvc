c74e17ba859e47118e9a59f0b5b388ad50f6ac0f
Yael Alejandro Santana Michel <51036254+Ikigire@users.noreply.github.com>
2023-11-13 21:33:31 -0600

    Cambios completos para webhosting

--- app/tareas/tareas.controller.php
+++ app/tareas/tareas.controller.php
@@ -5,7 +5,7 @@ if (!isset($path_components[$base_index + 1]))
 switch ($path_components[$base_index + 1]) {
     case 'mi-lista':
         if (!checkSession()) {
-            header("Location: /mvc/tareas/login ");
+            header("Location: /tareas/login ");
             exit();
         }
         require_once("./app/tareas/mi-lista/controller/mi-lista.controller.php");
@@ -13,7 +13,7 @@ switch ($path_components[$base_index + 1]) {
 
     case 'registro':
         if (!checkSession()) {
-            header("Location: /mvc/tareas/login ");
+            header("Location: /tareas/login ");
             exit();
         }
         require_once("./app/tareas/registro/controller/registro.controller.php");
@@ -21,32 +21,32 @@ switch ($path_components[$base_index + 1]) {
 
     case 'completar':
         if (!checkSession()) {
-            header("Location: /mvc/tareas/login ");
+            header("Location: /tareas/login ");
             exit();
         }
 
         if ( !isset( $query_params ) || !isset( $query_params['id'] ) ) {
-            header("Location: /mvc/tareas/mi-lista ");
+            header("Location: /tareas/mi-lista ");
             exit();
         }
         include_once("./app/tareas/repository/tareas.repository.php");
         $tarea = TareasRepository::getInstance()->getTareaById( $query_params['id'] );
 
         if ( !$tarea ){
-            header("Location: /mvc/tareas/mi-lista ");
+            header("Location: /tareas/mi-lista ");
             break;
         }
 
         $tarea->setStatus("Completado");
 
         if ( TareasRepository::getInstance()->editTarea( $tarea ) ) {
-            header("Location: /mvc/tareas/mi-lista ");
+            header("Location: /tareas/mi-lista ");
         }
         break;
 
     case 'login':
         if (checkSession()) {
-            header("Location: /mvc/tareas/mi-lista ");
+            header("Location: /tareas/mi-lista ");
             exit();
         }
         require_once("./app/tareas/login/controller/login.controller.php");
@@ -54,7 +54,7 @@ switch ($path_components[$base_index + 1]) {
 
     case 'registro_usuario':
         if (checkSession()) {
-            header("Location: /mvc/tareas/mi-lista ");
+            header("Location: /tareas/mi-lista ");
             exit();
         }
         require_once("./app/tareas/nuevo_usuario/controller/registro_usuario.controller.php");
@@ -62,7 +62,7 @@ switch ($path_components[$base_index + 1]) {
 
     case 'recovery':
         if (checkSession()) {
-            header("Location: /mvc/tareas/mi-lista ");
+            header("Location: /tareas/mi-lista ");
             exit();
         }
         require_once("./app/tareas/recovery/controller/recovery.controller.php");
@@ -70,7 +70,7 @@ switch ($path_components[$base_index + 1]) {
     
     case 'recovery-s2':
         if (checkSession()) {
-            header("Location: /mvc/tareas/mi-lista ");
+            header("Location: /tareas/mi-lista ");
             exit();
         }
         require_once("./app/tareas/recovery/controller/recovery-step2.controller.php");
